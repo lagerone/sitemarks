90b78b9da19aad90ba4de87504e68f2da5335d94
Marcus Ekelund <marcus.ekelund@gmail.com>
2013-06-29 16:44:02 +0200

    Gruntfile for automated minifying/testing added
    Tasks for jshint, uglify and jasmine. Also a minimal package.json for
    the grunt dependencies.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		jshint: {
+			all: ['Gruntfile.js', 'scripts/*.js']
+		},
+
+		uglify: {
+			all: {
+				files: {
+					'build/sitemarks.merged.js': ['scripts/sitemarks.app.js', 'script/sitemarks.storage.js']
+				}
+			}
+		},
+
+		jasmine: {
+			all: {
+				src: 'scripts/sitemarks.storage.js',
+				options: {
+					specs: 'scripts/sitemarks.storage.spec.js'
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+	grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']);
+};
