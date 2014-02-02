df6b58ba3cb70bb332ef734978c64626f1045163
Daniel lager <daniel.lager@gmail.com>
2014-02-02 10:50:55 +0100

    setup grunt less and watch to task dev

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -1,33 +1,49 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+	var config = {};
 
-		jshint: {
-			all: ['Gruntfile.js', 'scripts/*.js']
-		},
-
-		uglify: {
-			all: {
-				files: {
-					'build/sitemarks.merged.js': ['scripts/sitemarks.storage.js', 'scripts/sitemarks.oink.js', 'scripts/sitemarks.app.js']
-				}
-			}
-		},
-
-		jasmine: {
-			all: {
-				src: ['scripts/sitemarks.storage.js', 'scripts/sitemarks.oink.js'],
-				options: {
-					specs: 'scripts/*.spec.js'
-				}
+	config.jshint = {
+		all: ['Gruntfile.js', 'scripts/*.js']
+	};
+	config.uglify = {
+		all: {
+			files: {
+				'build/sitemarks.merged.js': ['scripts/sitemarks.storage.js', 'scripts/sitemarks.oink.js', 'scripts/sitemarks.app.js']
 			}
 		}
+	};
+	config.jasmine = {
+		all: {
+			src: ['scripts/sitemarks.storage.js', 'scripts/sitemarks.oink.js'],
+			options: {
+				specs: 'scripts/*.spec.js'
+			}
+		}
+	};
+	config.less = {
+		development: {
+			options: {
+				paths: ['less'],
+				compress: true
+			},
+			files: {
+				'css/sitemarks.min.css': 'css/sitemarks.less'
+			}
+		}
+	};
+	config.watch = {
+		files: 'css/*.less',
+		tasks: ['less']
+	};
 
-	});
+	grunt.initConfig(config);
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
+	grunt.registerTask('dev','watch');
 	grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']);
 };
